38714ec49ac7490f63ede330cab24dfbb75499a9
Raimo Radczewski <raimo@crftd.tech>
2024-05-22 17:12:42 +0200

    chore: Parse commandline

--- trunkver.go
+++ trunkver.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
 	"time"
 )
 
 func formatTrunkver(ts time.Time, sourceRef, buildRef string) string {
 	return ts.UTC().Format("20060102150405") + ".0.0-" + sourceRef + "-" + buildRef
+}
+
+func main() {
+	run(os.Stdout, os.Args)
+}
+
+func run(w io.Writer, args []string) {
+	flagSet := flag.NewFlagSet("trunkver", flag.ExitOnError)
+	ts := flagSet.String("timestamp", "now", "The timestamp to use for the version in RFC3339 format")
+	sRef := flagSet.String("source-ref", "", "The source ref to use for the version")
+	bRed := flagSet.String("build-ref", "", "The build ref to use for the version")
+
+	flagSet.Parse(args[1:])
+
+	var parsedTime time.Time
+	if *ts == "now" {
+		parsedTime = time.Now()
+	} else {
+		var err error
+		parsedTime, err = time.Parse(time.RFC3339, *ts)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Fprintln(w, formatTrunkver(parsedTime, *sRef, *bRed))
 }
--- trunkver_test.go
+++ trunkver_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io"
+	"os"
 	"testing"
 	"time"
 
@@ -16,5 +18,24 @@ func TestApplicationVersion(t *testing.T) {
 		"20240522142548.0.0-gb4dc0d3-12345",
 		formatTrunkver(now, "gb4dc0d3", "12345"),
 		"Should create a semver-compatible version",
+	)
+}
+
+func TestPassAllExplicitArgs(t *testing.T) {
+	args := []string{
+		"trunkver",
+		"--timestamp", "2024-05-22T16:25:48+02:00",
+		"--source-ref", "gb4dc0d3",
+		"--build-ref", "12345",
+	}
+	r, w, _ := os.Pipe()
+	run(w, args)
+	w.Close()
+
+	out, _ := io.ReadAll(r)
+	assert.Equal(t,
+		"20240522142548.0.0-gb4dc0d3-12345\n",
+		string(out),
+		"Should print the version to stdout",
 	)
 }
