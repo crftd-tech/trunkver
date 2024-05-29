cf4dd11adc741771fa50ebb0fadd6c69076b7bfd
Raimo Radczewski <raimo@crftd.tech>
2024-05-30 00:13:01 +0200

    fix: Coerce build- and source-ref to valid prerelease tokens

--- internal/trunkver/generate.go
+++ internal/trunkver/generate.go
@@ -1,11 +1,34 @@
 package trunkver
 
-import "time"
+import (
+	"regexp"
+	"time"
+)
+
+var sourceRefFilterRegex *regexp.Regexp = regexp.MustCompile(`[^a-zA-Z0-9]`)
+
+func coerceSourceRef(sourceRef string) string {
+	return sourceRefFilterRegex.ReplaceAllString(sourceRef, "")
+}
+
+var buildRefFilterRegex *regexp.Regexp = regexp.MustCompile(`[^a-zA-Z0-9-]`)
+
+func coerceBuildRef(buildRef string) string {
+	return buildRefFilterRegex.ReplaceAllString(buildRef, "-")
+}
 
 func GenerateMajorTrunkver(ts time.Time, sourceRef, buildRef string) string {
-	return ts.UTC().Format("20060102150405") + ".0.0-" + sourceRef + "-" + buildRef
+	trunkVer := ts.UTC().Format("20060102150405") + ".0.0-" + coerceSourceRef(sourceRef) + "-" + coerceBuildRef(buildRef)
+	if _, err := ParseTrunkVer(trunkVer); err != nil {
+		panic(err)
+	}
+	return trunkVer
 }
 
 func GeneratePrereleaseTrunkver(ts time.Time, sourceRef, buildRef string) string {
-	return ts.UTC().Format("20060102150405") + "-" + sourceRef + "-" + buildRef
+	trunkVer := ts.UTC().Format("20060102150405") + "-" + coerceSourceRef(sourceRef) + "-" + coerceBuildRef(buildRef)
+	if _, err := ParseTrunkVer(trunkVer); err != nil {
+		panic(err)
+	}
+	return trunkVer
 }
