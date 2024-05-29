dabb2d0fb7ced969a5029a8affcfface8aae6765
Raimo Radczewski <raimo@crftd.tech>
2024-05-30 00:18:46 +0200

    chore: Use semver to construct major version

--- internal/trunkver/generate.go
+++ internal/trunkver/generate.go
@@ -2,7 +2,10 @@ package trunkver
 
 import (
 	"regexp"
+	"strconv"
 	"time"
+
+	"github.com/Masterminds/semver/v3"
 )
 
 var sourceRefFilterRegex *regexp.Regexp = regexp.MustCompile(`[^a-zA-Z0-9]`)
@@ -18,7 +21,17 @@ func coerceBuildRef(buildRef string) string {
 }
 
 func GenerateMajorTrunkver(ts time.Time, sourceRef, buildRef string) string {
-	trunkVer := ts.UTC().Format("20060102150405") + ".0.0-" + coerceSourceRef(sourceRef) + "-" + coerceBuildRef(buildRef)
+	major, err := strconv.ParseUint(ts.UTC().Format("20060102150405"), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	var sVer semver.Version = *semver.New(
+		major, 0, 0,
+		coerceSourceRef(sourceRef)+"-"+coerceBuildRef(buildRef),
+		"",
+	)
+
+	trunkVer := sVer.String()
 	if _, err := ParseTrunkVer(trunkVer); err != nil {
 		panic(err)
 	}
