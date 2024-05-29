c70154a47e344a06edc72e49548fca0bc152c363
Raimo Radczewski <raimo@crftd.tech>
2024-05-29 23:51:26 +0200

    feature: Allow format and outputting to file (preparing GHA)

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"text/template"
 	"time"
 
 	"github.com/crftd-tech/trunkver/internal/ci"
@@ -20,6 +22,7 @@ var generateCmd = &cobra.Command{
 		var sourceRef string = cmd.Flags().Lookup("source-ref").Value.String()
 		var timestamp string = cmd.Flags().Lookup("timestamp").Value.String()
 		var prerelease bool = cmd.Flags().Lookup("prerelease").Value.String() == "true"
+		var fileOutput string = cmd.Flags().Lookup("output").Value.String()
 
 		ciResult, found := ci.DetectCi()
 		if found {
@@ -53,18 +56,33 @@ var generateCmd = &cobra.Command{
 			}
 		}
 
-		if !prerelease {
-			fmt.Fprintln(os.Stdout, trunkver.GenerateMajorTrunkver(parsedTime, sourceRef, buildRef))
-			return
+		var trunkVer string
+		if prerelease {
+			trunkVer = trunkver.GeneratePrereleaseTrunkver(parsedTime, sourceRef, buildRef)
+
+			baseVersionProvided := len(args) > 0 && args[0] != ""
+			if baseVersionProvided {
+				trunkVer = trunkver.MergeWithBaseVersion(args[0], trunkVer)
+			}
+		} else {
+			trunkVer = trunkver.GenerateMajorTrunkver(parsedTime, sourceRef, buildRef)
 		}
 
-		var trunkVer string = trunkver.GeneratePrereleaseTrunkver(parsedTime, sourceRef, buildRef)
-		if len(args) == 0 || args[0] == "" {
-			fmt.Fprintln(os.Stdout, trunkVer)
-			return
+		if format := cmd.Flags().Lookup("format").Value.String(); format != "" {
+			var tpl = template.Must(template.New("trunkver").Parse(format))
+			var buffer bytes.Buffer
+			if err := tpl.Execute(&buffer, trunkVer); err != nil {
+				panic(err)
+			}
+			trunkVer = buffer.String()
 		}
 
-		fmt.Fprintln(os.Stdout, trunkver.MergeWithBaseVersion(args[0], trunkVer))
+		fmt.Println(trunkVer)
+		if fileOutput != "" {
+			if err := os.WriteFile(fileOutput, []byte(trunkVer+"\n"), 0644); err != nil {
+				panic(err)
+			}
+		}
 	},
 }
 
@@ -72,6 +90,8 @@ func init() {
 	generateCmd.Flags().StringP("build-ref", "b", "", "The build ref to use (e.g. $GITHUB_RUN_ID)")
 	generateCmd.Flags().StringP("source-ref", "s", "", "The source ref to use for the version (e.g. \"g$(git rev-parse --short HEAD)\")")
 	generateCmd.Flags().StringP("timestamp", "t", "now", "The timestamp to use for the version in RFC3339 format")
+	generateCmd.Flags().StringP("output", "o", "", "Write TrunkVer to file")
+	generateCmd.Flags().StringP("format", "f", "", "Use template to format TrunkVer")
 	generateCmd.Flags().BoolP("prerelease", "p", false, "Build the TrunkVer as the prerelease part of a semver (e.g. for nightly builds)")
 
 	rootCmd.AddCommand(generateCmd)
